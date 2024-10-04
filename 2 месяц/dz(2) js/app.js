3137d143964574bc9ad5d9cf395560239101acfa
Владислав <manchelaev25062001@gmail.com>
2024-10-04 15:47:37 +0600

    Add files via upload

---
+++ 2 месяц/dz(2) js/app.js
@@ -0,0 +1,53 @@
+1;
+
+let number1 = Number(prompt("Введите первое число"));
+let number2 = Number(prompt("Введите второе число"));
+
+if (number1 > number2) {
+  console.log("Первое число больше");
+} else if (number1 < number2) {
+  console.log("Второе число больше");
+} else {
+  console.log(" Числа равны ");
+}
+
+// //   2.
+
+// let numberPlanet = Number(prompt("Напишите номер планеты"));
+// let NamePlanet;
+
+// if (numberPlanet === 1) {
+//   console.log((NamePlanet = "Меркурий"));
+// } else if (numberPlanet === 2) {
+//   console.log((NamePlanet = "Венера"));
+// } else if (numberPlanet === 3) {
+//   console.log((NamePlanet = "Земля"));
+// } else if (numberPlanet === 4) {
+//   console.log((NamePlanet = "Mapc"));
+// } else if (numberPlanet === 5) {
+//   console.log((NamePlanet = "Юпитер"));
+// } else if (numberPlanet === 6) {
+//   console.log((NamePlanet = "Сатурн"));
+// } else if (numberPlanet === 7) {
+//   console.log((NamePlanet = "Уран"));
+// } else if (numberPlanet === 8) {
+//   console.log((NamePlanet = "Нептун"));
+// } else if (numberPlanet === 9) {
+//   console.log((NamePlanet = "Плутон"));
+// } else {
+//   console.log("Error");
+// }
+
+// 3.
+
+// let student = {
+//   name: "Sasha", // имя
+//   surName: "Kurchatov", // Фамилия
+//   age: 22, // возраст
+//   post: "student", // студент
+//   workAtNight: true, // работает ли по ночам
+//   wage: 50_000, // зарплата
+//   address: "Moskovskaya 30b", // адрес работы
+// };
+
+// console.log(student);
